100cde4b8fa7744225e8e0035cc0f33cfeba85f8
Your Namealt1tla <GerdzhikovaVeronWench@gmail.com>
2024-02-06 16:42:32 +0300

    laba1 done

--- Hello, World!/index.php
+++ Hello, World!/index.php
@@ -4,14 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veronika Gerdzhikova 231-322</title>
+    <style>
+    header{
+        display:flex;
+    }
+    span{
+        display:block;
+        margin:auto;
+        font-size: 3rem;
+        color: #0b0b0b;
+    }
+    </style>
 </head>
 <body>
     <header>
+        <img src=".img/logo.png" alt="moscow polytech logo" width='300px' height='300px'>
+        <span>Hello, World!</span>
     </header>
     <main>
-        <?php?>
+        <?php echo 'Hello, Wolrd!' ?>
     </main>
     <footer>
+        <span>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий</span>
     </footer>
 </body>
 </html>
