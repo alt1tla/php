642249429b64c1c91709de560c1e2f2c5e0d1a21
Your Namealt1tla <GerdzhikovaVeronWench@gmail.com>
2024-02-13 13:12:49 +0300

    laba 2 done

---
+++ Feedback_form/back.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veronika Gerdzhikova 231-322</title>
+</head>
+<body>
+    <textarea name="ta1" id="ta1" cols="100" rows="100">
+        <?php
+            print_r(get_headers("https://docs.google.com/spreadsheets/d/1wo72S6p5xXMA93kFcQlJh0pAclYphX3WimbCmqRzWJI/edit#gid=649063110"));
+        ?>
+    </textarea>
+    <a href="index.html">back</a>
+</body>
+</html>
