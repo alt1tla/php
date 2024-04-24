ef3e6d2d7fafd1b6c5ea3bc70342849f2805b96b
Your Namealt1tla <GerdzhikovaVeronWench@gmail.com>
2024-04-24 09:11:10 +0300

    folder cookies added

---
+++ Cookies/cookie.php
@@ -0,0 +1,25 @@
+<form action="" method="GET">
+	<input type="email" name="email">
+	<input type="submit">
+</form>
+
+<?php
+	if (!empty($_REQUEST['email'])) {
+		session_start(); 
+		$_SESSION['email'] = $_REQUEST['email']; 
+	}
+?>
+<?php
+	if(!empty($_SESSION['email']))
+		$email = $_SESSION['email'];
+	else
+		$email = '';
+?>
+
+<form action="" method="GET">
+	<input type="text" name="name">
+	<input type="text" name="surname">
+	<input type="text" name="phone" value="<?php echo $email ?>">
+
+	<input type="submit">
+</form>
